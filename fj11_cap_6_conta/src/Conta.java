de2f3043939614492dfe502a9f8b1b0bbc990ec7
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2025-06-03 22:42:16 -0400

    Criação do primeiro projeto oficial do eclipse pelo fj11 - bem básico

---
+++ fj11_cap_6_conta/src/Conta.java
@@ -0,0 +1,13 @@
+
+public class Conta {
+	private double saldo;
+
+	void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public double getSaldo() {
+		// TODO Auto-generated method stub
+		return this.saldo;
+	}
+}
