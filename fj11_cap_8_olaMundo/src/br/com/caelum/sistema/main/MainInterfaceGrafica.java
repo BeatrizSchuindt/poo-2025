d4a4a8a600297f137969ee43544e524ee7ffcc37
BeatrizSchuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2025-06-11 15:06:32 -0400

    Adicionando minhas classes do FJ11

---
+++ fj11_cap_8_olaMundo/src/br/com/caelum/sistema/main/MainInterfaceGrafica.java
@@ -0,0 +1,9 @@
+package br.com.caelum.sistema.main;
+
+import br.com.caelum.javafx.api.main.OlaMundo;
+
+public class MainInterfaceGrafica {
+	public static void main(String[] args) {
+		OlaMundo.main(args);
+	}
+}
